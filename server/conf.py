2cad79f1bae2dda982dd9f41b8bc070859397350
Oleg Silkin <o.silkin98@gmail.com>
2019-05-19 02:23:30 -0400

    Config file

---
+++ server/conf.py
@@ -0,0 +1,9 @@
+project_dir = '/home/oleg/PycharmProjects/comment-server'
+schema_dir = '/'.join((project_dir, 'schema/comments_ddl.sql',))
+database_dir = '/'.join((project_dir, 'example.db',))  # change this
+
+
+anonymous = {
+    'channel_id': '9cb713f01bf247a0e03170b5ed00d5161340c486',
+    'channel_name': '@Anonymous'
+}
