9f28ec04b4cb2d49b65f4bcce126ab2c7a8690d6
Oleg Silkin <o.silkin98@gmail.com>
2019-05-20 01:17:56 -0400

    Adds backup code + fixes obtain comment by reply code

--- server/conf.py
+++ server/conf.py
@@ -1,7 +1,7 @@
 project_dir = '/home/oleg/PycharmProjects/comment-server'
 schema_dir = '/'.join((project_dir, 'schema/comments_ddl.sql',))
 database_dir = '/'.join((project_dir, 'example.db',))  # change this
-
+backup_dir = '/'.join((project_dir, 'example.backup.db'))
 
 anonymous = {
     'channel_id': '9cb713f01bf247a0e03170b5ed00d5161340c486',
